e2a6f650419bdec200fa511ded7c0716d6645747
Matias Nicolas Michelli <proga2332@gmail.com>
2022-10-12 00:19:06 -0300

    README update

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CarsModule } from './cars/cars.module';
-import { FlavorsController } from './flavors/flavors.controller';
-import { FlavorsModule } from './flavors/flavors.module';
 
 @Module({
-  imports: [CarsModule, FlavorsModule],
+  imports: [CarsModule],
   controllers: [AppController],
   providers: [AppService],
 })
